6f9a2c9849bca80b5e5be09a0106379fee8a9fc0
elshora97 <A.elshora@dataserve.com.sa>
2024-09-23 02:00:01 +0300

    Add Error Page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,13 +1,32 @@
 import { Link, useRouteError } from "react-router-dom";
+import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
   const error = useRouteError();
   console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img
+            src={img}
+            alt="not found"
+          />
+          <h3>Ohh! page not found</h3>
+          <p>We can&apos;t seem to find the page you&apos;re looking for</p>
+          <Link to="/dashboard"> back home </Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
-    <div>
-      <h1>Error page</h1>
-      <Link to="/"> back home </Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>There was an error</h3>
+      </div>
+    </Wrapper>
   );
 };
 
