13f4b493a195a6339b7c34697f556cf8eac316d9
elshora97 <A.elshora@dataserve.com.sa>
2024-09-22 19:48:47 +0300

    Add pages Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomeLayout, Register, Login, DashboardLayout } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
