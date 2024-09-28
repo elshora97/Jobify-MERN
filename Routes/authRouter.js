3d7d1349f9c216f61ec049cb78485b5492ae4215
elshora97 <A.elshora@dataserve.com.sa>
2024-09-28 20:17:09 +0300

    Update Register Controller

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { register, login } from "../controllers/authController.js";
 const router = Router();
 
-router.post("/register", register);
+import { register, login } from "../controllers/authController.js";
+import { validateRegisterInput } from "../middleware/validationMiddleware.js";
+
+router.post("/register", validateRegisterInput, register);
 router.post("/login", login);
 
 export default router;
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,14 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 export const register = async (req, res) => {
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? "admin" : "user";
+
+  req.body.password = await hashPassword(req.body.password);
+
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
