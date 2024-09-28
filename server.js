853eb190ba1418de4163b582429bd3cb8dd55316
elshora97 <A.elshora@dataserve.com.sa>
2024-09-28 17:32:46 +0300

    Add Job Model

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
-import { nanoid } from "nanoid";
+import mongoose from "mongoose";
 import jobRouter from "./Routes/jobRouter.js";
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -17,18 +16,25 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hello world ...");
 });
-
 app.use("/api/v1/jobs", jobRouter);
-
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
 });
-
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).json({ msg: "something went wrong" });
 });
 
-app.listen(port, () => {
-  console.log(`server is listening on port ${port} ....`);
-});
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port} ....`);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+connect();
