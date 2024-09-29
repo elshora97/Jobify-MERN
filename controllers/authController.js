590e0640b1ce89e60fd9f721f5620685c6cd6542
elshora97 <A.elshora@dataserve.com.sa>
2024-09-29 19:05:45 +0300

    Add Login Logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,29 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
-import bcrypt from "bcryptjs";
-import { hashPassword } from "../utils/passwordUtils.js";
+import { hashPassword, comparePassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customError.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
-
   req.body.password = await hashPassword(req.body.password);
-
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
 
 export const login = async (req, res) => {
-  res.send("register");
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+  const isValidUser = user && (await comparePassword(password, user.password));
+  if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
+
+  const token = createJWT({ userId: user.id, role: user.role });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie("token", token, {
+    expiresIn: oneDay,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  });
+  res.status(StatusCodes.OK).json({ msg: "User Loged in" });
 };
