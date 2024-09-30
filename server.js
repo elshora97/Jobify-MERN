729b15f966de79da02c9bc5610e8cb14ee109da1
elshora97 <A.elshora@dataserve.com.sa>
2024-09-30 18:06:18 +0300

    Add User Routes Setup

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import cookieParser from "cookie-parser";
 //routers
 import jobRouter from "./Routes/jobRouter.js";
 import authRouter from "./Routes/authRouter.js";
+import userRouter from "./Routes/userRouter.js";
 
 //middlewares
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -29,6 +30,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
