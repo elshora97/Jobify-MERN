cbdf334c7b6b03bde671ec62749445d0ece49e35
elshora97 <A.elshora@dataserve.com.sa>
2024-10-05 17:37:40 +0300

    Add Admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import {
   Profile,
   Stats,
   EditJob,
+  Admin,
 } from "./pages";
 
 import { action as registerAction } from "./pages/Register";
@@ -22,6 +23,7 @@ import { action as deleteJobAction } from "./pages/DeleteJob";
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
 import { loader as editJobLoader } from "./pages/EditJob";
+import { loader as adminLoader } from "./pages/Admin";
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -69,6 +71,11 @@ const router = createBrowserRouter([
           {
             path: "profile",
             element: <Profile />,
+          },
+          {
+            path: "admin",
+            element: <Admin />,
+            loader: adminLoader,
           },
           {
             path: "stats",
