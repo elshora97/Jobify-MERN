6ae69d3ea5cc01a5f545322945ad9c94b913303f
elshora97 <A.elshora@dataserve.com.sa>
2024-10-05 19:58:17 +0300

    Add stats page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -12,3 +12,5 @@ export { default as SearchContainer } from "./SearchContainer";
 export { default as Job } from "./Job";
 export { default as StatItem } from "./StatItem";
 export { default as SubmitBtn } from "./SubmitBtn";
+export { default as StatsContainer } from "./StatsContainer";
+export { default as ChartsContainer } from "./ChartsContainer";
